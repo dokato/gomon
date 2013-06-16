49cd431e78a8b3fec6b1092b7a1fe5adef1e96fa
dokato <moj@mail.com>
2013-06-16 08:17:02 +0200

    komentarze dodano komentarze z opisami dzialania poszczeguolnych klas, funkcji i zmiennych. pomoze to usprawniac kod w przyszlosc. nie zmieniono niczego w programistycznej czesci kodu.

--- gomokuqt_new.py
+++ gomokuqt_new.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+__author__ = 'Dominik Krzeminski'
+__version__ = '0.82'
+__date__ = '29-05-2013'
+__license__ = 'GPL'
+
 import sys
 from socket import *
 import numpy as np
@@ -11,12 +16,12 @@ class Gra(QtGui.QMainWindow):
 	def __init__(self):
 		super(Gra, self).__init__()
 		#inicjalizacja podsatwowych stalych
-		self._prz=45
-		self._x0=10
+		self._prz=45 #wymiary buttona
+		self._x0=10 # poczotkowe pozycje
 		self._y0=40
-		self.wym=12
-		self.stan=StatusGry()
-		self.komp=SAI()
+		self.wym=12 # wymiar planszy
+		self.stan=StatusGry() #klasa sprawdzajaca czy rozgrywka jest juz skonczona
+		self.komp=SAI() # prosta sztuczna inteligencja - wybory komputera
 		
 		self.resetuj(self.wym)
 		
@@ -32,6 +37,9 @@ class Gra(QtGui.QMainWindow):
 		self.show()
 	
 	def resetuj(self,wym2=12):
+		'''resetuje plansze zmienia status gry online i z komputerem
+		na falsz, zmienia licznik ruchu na 1, jezeli problem z wymiarem
+		to blokuje przyciski'''
 		self.znak=1 # 1 kolko, 0 krzyzyk, 2 puste
 		
 		self.ONLINE_ST=False
@@ -44,12 +52,14 @@ class Gra(QtGui.QMainWindow):
 				self.przyc[i].setEnabled(False)
 
 	def reset_planszy(self):
+		'czysci przyciski na puste'
 		self.resetuj()
 		for i in range(len(self.przyc)):
 			self.przyc[i].setText(' ')
 		self.blokada(True)
 		
 	def gorne_menu(self):
+		'obsluguje wszystkie akcje z gornego menu'
 		menubar = self.menuBar()
 		
 		exitAct = QtGui.QAction('&Zamknij', self)        
@@ -100,8 +110,7 @@ class Gra(QtGui.QMainWindow):
 	def wybor_rozm(self):
 		items = {"8x8":8, "12x12":12, "15x15":15}
 
-		item, ok = QtGui.QInputDialog.getItem(self, "Rozmiar",
-			"Season:", items.keys(), 0, False)
+		item, ok = QtGui.QInputDialog.getItem(self, "Rozmiar", "Wybierz:", items.keys(), 0, False)
 		if ok and item:
 			self.statusBar().showMessage('Wybrano rozmiar: ' +str(item))
 			self.resetuj(items[str(item)])
@@ -111,6 +120,8 @@ class Gra(QtGui.QMainWindow):
 		QtGui.QMessageBox.about(self, "O Gomoku", msg.strip())
 
 	def plansza(self,n):
+		'''obsluguje stan poczatkowy planszy, rysuje przyciski i dodaje
+		do nich mozliwosc klikniecia wraz z odpowiednia akcja'''
 		self.czyjruch()
 
 		self.przyc=[]
@@ -123,12 +134,13 @@ class Gra(QtGui.QMainWindow):
 				self.przyc[-1].pozycja=(i,j)
 
 	def klik(self):
+		'czyli co sie dzieje po kliknieciu - przechwytuje pozycje i wywoluje wstaw'
 		sender = self.sender()
-		print self.licznikruchu
 		zl,zr=sender.pozycja
 		self.wstaw(zl,zr)
 	
 	def rysuj(self,plansza):
+		'rysowanie planszy dostosowane do nowej sytuacji na niej'
 		l=0
 		for i in xrange(plansza.shape[0]):
 			for j in xrange(plansza.shape[0]):
@@ -141,20 +153,18 @@ class Gra(QtGui.QMainWindow):
 				l+=1
 
 	def wstaw(self,a1,a2):
+		'''wstawianie nowego ruchu na plansze wraz z obsluga kilku zdarzen
+		m.in. przypadkow wyboru online, badz wyboru gry z komputerem'''
 		if self.ls[a1][a2]==2:
 			self.licznikruchu+=1
 			self.ls[a1][a2]=self.znak
 			
 			if self.AI==True:
 				self.licznikruchu+=1
-				
 				sz1,sz2=self.komp.simple_ai(self.ls)
-				
 				self.znak-=1
-				
 				self.ls[sz1][sz2]=self.znak
 				self.rysuj(self.ls)
-				
 			else:
 				self.rysuj(self.ls)
 
@@ -172,6 +182,7 @@ class Gra(QtGui.QMainWindow):
 
 	###ONLINE
 	def startgracz1(self):
+		'wysyla komunikat o poczatku gry i czeka na polaczenie'
 		self.reset_planszy()
 		self.ONLINE_ST=True
 		self.blokada(False)
@@ -182,6 +193,7 @@ class Gra(QtGui.QMainWindow):
 			self.blokada(True)
 
 	def startgracz2(self):
+		'jesli odbierze komunikat o poczatku powinna sie zaczac tura online'
 		self.reset_planszy()
 		self.blokada(False)
 		self.ONLINE_ST=True
@@ -192,12 +204,14 @@ class Gra(QtGui.QMainWindow):
 			self.odbierz_syg()
 
 	def wyslij_syg(self,z1,z2):
+		'wysyla kliknieta pozycje'
 		self.blokada(False)
 		self.wo.przekaz(str((z1,z2)))
 		self.odbierz_syg()
 		self.blokada(True)
 
 	def odbierz_syg(self):
+		'nasluchuje, a gdy obierze powinno wstawic i przejsc do wyslania'
 		self.blokada(False)
 		dostane=self.wo.odbierz()
 		while dostane!='start':
@@ -208,10 +222,12 @@ class Gra(QtGui.QMainWindow):
 		self.blokada(True)
 	###
 	def start_z_komp(self):
+		'rozpoczyna gre z AI, resetuje plansze i ustawia status AI'
 		self.reset_planszy()
 		self.AI=True
 
 	def koniec(self):
+		'info o wygranej na koncu z uzyciem status baru'
 		if self.znak==1:
 			self.statusBar().showMessage('Wygral krzyzyk')
 		if self.znak==0:
@@ -219,16 +235,23 @@ class Gra(QtGui.QMainWindow):
 		self.blokada()
 
 	def blokada(self,czy=False):
+		'wylacza przyciski'
 		for i in range(len(self.przyc)):
 			self.przyc[i].setEnabled(czy)
 
 	def czyjruch(self):
+		'info o kolejnosci ruchu'
 		if self.znak==1:
 			self.statusBar().showMessage('Ruch kolka')
 		if self.znak==0:
 			self.statusBar().showMessage('Ruch krzyzyka')
 
 class WysOdb(object):
+	'''Klasa obsugujaca serwer/klient w grze. Przy inicie szuka wolnego
+	portu zeby postawic serwer. Przy przekaz szuka wolnego portu dla klienta.
+	Zapamietuje te porty. Potem wysyla przez przekaz i odbiera przez odbierz.
+	Sygnalem poprawnosci ma byc slowko <<ok>> wyslane przy odebraniu.
+	Jako czat dziala bez zarzutu'''
 	def __init__(self,port_srv=41000,port_kl=42000,host='localhost',op=1):
 		self.s = socket(AF_INET, SOCK_STREAM)
 
@@ -271,6 +294,10 @@ class WysOdb(object):
 	
 		
 class StatusGry():
+	'''begin - inicjalizuje plansze
+	checkit przebiega po wierszach, kolumnach, przekatnych aby sprawdzicz czy nie
+	ma liczby <<lim>> takich samych w danych rzucie. Jesli jest to zwraca False,
+	gdy nie to oddaje True'''
 	def begin(self, n):
 		x=np.ones((n,n))*2
 		return x
@@ -307,10 +334,14 @@ class StatusGry():
 		return True
 
 class SAI(object):
+	'''Bardzo proste AI. W zasadzie opiera sie o losowe ruchy. Tylko kiedy
+	przeciwnik ma ustawione 3/4 w rzedzie to stawia znak dookola przeciwnika
+	co moze zablokowac jego ruch.'''
 	def __init__(self):
 		self.sg=StatusGry()
 		
 	def simple_ai(self,plan):
+		self.cala_plansza=plan
 		if self.sg.checkit(plan,lim=4)==0:
 			return self.zakanczacz(plan)
 		else:
@@ -324,10 +355,26 @@ class SAI(object):
 			if plan[a][b]==2:
 				fl=1
 		if x==None and y==None: return a,b
-		else: return x+b-1,y+a+1
-	
+		else:
+			fl=0
+			nx,ny=x+b-1,y+a+1
+			while fl==0:
+				print nx,ny
+				print plan
+				if self.cala_plansza[nx][ny]==2:
+					return nx,ny
+				else:
+					a=np.random.randint(0,2)
+					if a==0: a-=1
+					nx+=a*np.random.randint(0,2)
+					ny+=a*np.random.randint(0,2)
+					if nx>self.cala_plansza.shape[0]: nx-=self.cala_plansza.shape[0]
+					if ny>self.cala_plansza.shape[0]: ny-=self.cala_plansza.shape[0]
+					if nx<0: nx+=self.cala_plansza.shape[0]
+					if ny<0: ny+=self.cala_plansza.shape[0]
+
 	def zakanczacz(self,plan):
-		lim=4
+		lim=3
 		for	e,row in enumerate(plan):
 			for i in xrange(len(row)):
 				if self.sg.sprawdzliste(row[i:i+lim],lim)==0:
@@ -337,7 +384,7 @@ class SAI(object):
 				if self.sg.sprawdzliste(row[i:i+lim],lim)==0:
 					return self.stupid_ai(plan[i:i+lim,e:e+lim],e,i)
 		for b in range(plan.shape[0]):
-			for i in range(len(plansza[0])):
+			for i in range(len(plan[0])):
 				tab=plan[b:b+lim, i:i+lim]
 				if tab.shape==(lim,lim):
 					if self.sg.sprawdzliste(tab.diagonal(),lim)==0:
